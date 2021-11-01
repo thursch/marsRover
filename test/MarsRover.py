e57b40c6af01218ae1b2190abd040b9ee74edac1
thursch <myriam.thursch@zuhlkw.com>
2021-11-01 23:20:17 +0000

    refactoring turnLeft method

--- test/MarsRover.py
+++ test/MarsRover.py
@@ -5,19 +5,13 @@ class MarsRover(object):
         self.yposition = yposition
 
     def turnRight(self):
-        compass = ["N", "E", "S", "W"]
-        self.direction = compass[(compass.index(self.direction) + 1) % 4]
+        compassClockwise = ["N", "E", "S", "W"]
+        self.direction = compassClockwise[(compassClockwise.index(self.direction) + 1) % 4]
 
 
     def turnLeft(self):
-        if self.direction == "N":
-            self.direction = "W"
-        elif self.direction == "W":
-            self.direction = "S"
-        elif self.direction == "S":
-            self.direction = "E"
-        else:
-            self.direction = "N"
+        compassAnticlockwise = ["N", "W", "S", "E"]
+        self.direction = compassAnticlockwise[(compassAnticlockwise.index(self.direction) + 1) % 4]
 
     def moveForward(self):
         if self.direction == "N":
